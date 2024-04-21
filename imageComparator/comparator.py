8dfc95a89a08556d4d2755512484f218c82eaf6d
Trhe_piernas <hruizdeegino@gmail.com>
2024-04-21 16:06:54 +0200

    cambiar metodo de comparacion de imagenes a metodo comparación de características

--- imageComparator/comparator.py
+++ imageComparator/comparator.py
@@ -1,27 +1,38 @@
 import cv2
-import numpy as np
+import os
+import shutil
 
 def comparar_imagenes(img1_path, img2_path):
-    # Cargar las imágenes en escala de grises
-    img1 = cv2.imread(img1_path, cv2.IMREAD_GRAYSCALE)
-    img2 = cv2.imread(img2_path, cv2.IMREAD_GRAYSCALE)
+    img_path = "./imagenes/"
+    # Cargar las dos imágenes
+    img1 = cv2.imread(img_path + img1_path, cv2.IMREAD_GRAYSCALE)
+    img2 = cv2.imread(img_path + img2_path, cv2.IMREAD_GRAYSCALE)
 
-    # Calcular los histogramas de las imágenes
-    hist1 = cv2.calcHist([img1], [0], None, [256], [0,256])
-    hist2 = cv2.calcHist([img2], [0], None, [256], [0,256])
+    # Inicializar el detector ORB
+    orb = cv2.ORB_create()
 
-    # Normalizar los histogramas
-    hist1 = cv2.normalize(hist1, hist1).flatten()
-    hist2 = cv2.normalize(hist2, hist2).flatten()
+    # Detectar y calcular las características
+    kp1, des1 = orb.detectAndCompute(img1, None)
+    kp2, des2 = orb.detectAndCompute(img2, None)
 
-    # Calcular la correlación entre los histogramas
-    correlacion = cv2.compareHist(hist1, hist2, cv2.HISTCMP_CORREL)
+    # Inicializar el matcher de fuerza bruta
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-    # Devolver la correlación
-    return correlacion
+    # Realizar el matching
+    matches = bf.match(des1, des2)
+
+    # Ordenar los matches por distancia
+    matches = sorted(matches, key = lambda x:x.distance)
+
+    # Devolver el número de matches
+    return len(matches)
 
 # Ejemplo de uso
-img1_path = "./imagenes/coche1.jpg"
-img2_path = "./imagenes/coche3.jpg"
-correlacion = comparar_imagenes(img1_path, img2_path)
-print("Correlación entre las imágenes:", correlacion)
+img1 = "coche2.jpg"
+img2 = "coche1.jpg"
+matches = comparar_imagenes(img1, img2)
+print("Número de matches entre las imágenes:", matches)
+# if matches > 100:
+#     os.makedirs("./coincidencias/1", exist_ok=True)
+#     shutil.move("./imagenes/" + img1, "./coincidencias/1/" + img1)
+#     shutil.move("./imagenes/" + img2, "./coincidencias/1/" + img2)
